bf32a164852f47fe9658f9ef0755e2ee3cfbbc19
NiRuaG <gaurin1107@gmail.com>
2018-12-21 23:51:34 -0600

    passport local strat, wip

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,58 +1,43 @@
 require("dotenv").config();
 const router = require("express").Router();
 
-
 router.route("/")
 
   .all((req, res, next) => {
-    console.log("\t\t@routes/admin ALL on /admin, req.body:\n", req.body);
+    console.log("\n\t\t@routes/admin ALL on /admin\n\treq.body:\n", req.body, '\n\t.headers.authoriztion\n', req.headers.authorization);
 
 
-    // -----------------------------------------------------------------------
-    // authentication middleware
-
-    const auth = { password: 'yourpassword' }; // change this
-
-    // parse login and password from headers
-    console.log("auth ===", req.headers.authorization);
-    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
-    console.log("b64?", b64auth);
-    const [login, password] = new Buffer(b64auth, 'base64').toString().split(':');
-    console.log("log/pw", login, password);
-
-    // Verify login and password are set and correct
-    if (!login || !password || password !== auth.password) {
-      // res.set('WWW-Authenticate', 'Basic realm="401"'); // change this
-      res.status(401).send('Authentication required.'); // custom message
-      return;
+    // console.log("auth ===", req.headers.authorization);
+    if (req.headers.authorization === undefined) {
+      //% When there is no attempt to provide authorization 
+      //% This likely occurs when first accessing the page 
+      //TODO: maybe change what responds
+      return res.render('admin', { authorized: false });
     }
 
-    next();
-
-
-
-
-  /*   if (req.body.password === undefined || req.body.password !== process.env.ADMIN_PW) {
+    if (req.headers.authorization !== process.env.AUTH_CHECK) {
       console.log("\t\t -- does not have a matching admin password");
-      return res.send("Unauthorized").status(401).end();
+      res.set('WWW-Authenticate', 'Basic realm="Administrator\'s Page"'); 
+      return res.status(401).render('admin', { authorized: false });
     }
-
     console.log("\t\t -- does have a matching admin password");
 
-    next(); */
+    next();
+    // const decode = Buffer.from(req.headers.authorization.split(' ')[1] || '', 'base64').toString('binary');
+    // console.log("decode:\n", decode);
   })
 
   .get((req, res) => {
-    console.log("\t\t@routes/admin GET on /admin, req.body:\n", req.body);
-    res.send("get admin page");
+    console.log("\n\t\t@routes/admin GET on /admin, req.body:\n", req.body);
+    return res.status(200).render('admin', { authorized: true });
   })
 
   .post((req, res) => {
-    console.log("\t\t@routes/admin POST on /admin, req.body:\n", req.body);
+    console.log("\n\t\t@routes/admin POST on /admin, req.body:\n", req.body);
     res.send("post admin page");
   })
 
-  ;
+;
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
+const passport = require('passport');
+const { Strategy } = require('passport-local');
+
 var db = require("./models");
 
 var app = express();
@@ -11,6 +14,33 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(require('cookie-parser')());
+
+app.disable('x-powered-by');
+
+passport.use(new Strategy(
+  function (username, password, cb) {
+    if (username !== 'admin') {
+      return cb(null, false);
+    }
+    if (process.env.ADMIN_PW != password) {
+      return cb(null, false);
+    }
+
+    return cb(null, true);
+  }));
+
+passport.serializeUser(function (user, cb) {
+  cb(null, process.env.ADMIN_ID);
+});
+
+passport.deserializeUser(function (id, cb) {
+  if (id !== process.env.ADMIN_ID) {
+    cb(null, false);
+  }
+  cb(null, true);
+});
+
 
 // Handlebars
 app.engine(
@@ -21,10 +51,12 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
+
 // Routes
 require("./routes/apiRoutes")(app);
 app.use("/admin", require("./routes/adminRoutes"));
-app.use("/"     , require("./routes/htmlRoutes" ));
+app.use("/", require("./routes/htmlRoutes"));
+
 
 var syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
@@ -33,9 +65,14 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
+
+
+process.env.AUTH_CHECK = `Basic ${Buffer.from(`admin:${process.env.ADMIN_PW}`, 'binary').toString('base64')}`;
+
+
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
