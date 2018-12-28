00a38d3f928309cab6ae6f26a85178958cb3aea4
NiRuaG <gaurin1107@gmail.com>
2018-12-27 19:23:13 -0600

    revert changes on index.js, style.css, index.handlebars

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
   // Get references to page elements
-  var $exampleText = $("#example-text");
-  var $exampleDescription = $("#example-description");
-  var $submitBtn = $("#submit");
-  var $exampleList = $("#example-list");
 
   // The API object contains methods for each kind of request we'll make
   var API = {
@@ -31,71 +27,226 @@ $(document).ready(function () {
     }
   };
 
-  // refreshExamples gets new examples from the db and repopulates the list
-  var refreshExamples = function () {
-    API.getExamples().then(function (data) {
-      var $examples = data.map(function (example) {
-        var $a = $("<a>")
-          .text(example.text)
-          .attr("href", "/example/" + example.id);
-
-        var $li = $("<li>")
-          .attr({
-            class: "list-group-item",
-            "data-id": example.id
-          })
-          .append($a);
-
-        var $button = $("<button>")
-          .addClass("btn btn-danger float-right delete")
-          .text("ｘ");
-
-        $li.append($button);
-
-        return $li;
-      });
-
-      $exampleList.empty();
-      $exampleList.append($examples);
-    });
-  };
 
   // handleFormSubmit is called whenever we submit a new example
-  // Save the new example to the db and refresh the list
-  var handleFormSubmit = function (event) {
-    event.preventDefault();
 
-    var example = {
-      text: $exampleText.val().trim(),
-      description: $exampleDescription.val().trim()
-    };
+  // Initialize economy values
 
-    if (!(example.text && example.description)) {
-      alert("You must enter an example text and description!");
-      return;
+  let currentBalance = 1000;
+  let startingBalance = 1000;
+  let balanceSpent = 0;
+  let netChange = 0;
+
+  // Insert values into finance report 
+
+  $("#currentBalance").text(currentBalance);
+  $("#startingBalance").text(startingBalance);
+  $("#balanceSpent").text(balanceSpent);
+  $("#netChange").text(netChange);
+
+  // Initialize functions for changing transTotal values
+
+
+  let coin1 = 0;
+  let coin2 = 0;
+  let coin3 = 0;
+
+
+
+  // On click buttons, execute function
+
+  $("#minus1").click(function() {
+
+    coin1--; 
+    $("#transTotal1").text(coin1);
+
+  });
+
+  $("#plus1").click(function() {
+
+    coin1++;
+    $("#transTotal1").text(coin1);
+
+  });
+
+  $("#minus2").click(function() {
+
+    coin2 --; 
+
+    $("#transTotal2").text(coin2);
+
+  });
+
+  $("#plus2").click(function() {
+
+    coin2 ++;
+    $("#transTotal2").text(coin2);
+
+  });
+
+  $("#minus3").click(function() {
+
+    coin3 --; 
+
+    $("#transTotal3").text(coin3);
+
+  });
+
+  $("#plus3").click(function() {
+
+    coin3 ++;
+    $("#transTotal3").text(coin3);
+
+  });
+
+
+
+  // COIN BANK OBJECT
+
+  let coinNames = [];
+
+  let coinValues = [];
+
+  // populate coinBank object on documentReady
+
+  function namePopulate() {
+
+    // check how many coins are on the page    
+
+    $(".coinName").each(function(index) {
+      console.log ( index + ": " + $(this).text() );
+
+      let name = $(this).text();
+
+      coinNames.push(name);
+
+      console.log(coinNames);
+
+
+      $("#nonsense").each(function(index) {
+
+        console.log ( index + ": " + $(this).text() );
+  
+        let value = $(this).text();
+  
+        coinValues.push(value);
+  
+        console.log(coinValues);
+  
+  
+      });
+
+
+    });
+
+    
+
+  }
+
+
+
+  namePopulate();
+
+
+  // coin bank init
+
+  let coinsBank = [];
+
+
+  // PURCHASE FUNCTION
+
+  function buyCoins() {
+
+    // For each coin
+
+    for (i = 0; i < coinNames.length; i++) {
+
+      // Subtract price from current balance
+
+      currentBalance = currentBalance - coinValues[i];
+
+      // add coin to coin bank
+
+      let deposit = {
+        
+        name: coinNames[i],
+        value: coinValues[i],
+        number: coin1
+      };
+
+      coinsBank.push(deposit);
+
+      console.log(coinsBank);
+
     }
 
-    API.saveExample(example).then(function () {
-      refreshExamples();
-    });
+    for (i=0; i<coinNames.length; i++) {
+      let j = i + 1;
 
-    $exampleText.val("");
-    $exampleDescription.val("");
+      // change modal values
+
+      $("#modalTotal" + j).text(0);
+      $("transTotal" + j).text(0);
+
+    }
+
+  }
+
+
+
+  // MODAL INFO
+
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("confirmTrans");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal 
+  btn.onclick = function() {
+
+    for (i=0; i<coinNames.length; i++) {
+      let j = i + 1;
+
+      // change modal values
+
+      let value = "#transTotal" + j;
+
+      let purchaseNum = $(value).text();
+
+      console.log(value);
+
+      $("#modalTotal" + j).text(purchaseNum);
+
+    }
+    modal.style.display = "block";
   };
 
-  // handleDeleteBtnClick is called when an example's delete button is clicked
-  // Remove the example from the db and refresh the list
-  var handleDeleteBtnClick = function () {
-    var idToDelete = $(this)
-      .parent()
-      .attr("data-id");
-
-    API.deleteExample(idToDelete).then(function () {
-      refreshExamples();
-    });
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
   };
 
-  // Add event listeners to the submit and delete buttons
-  $submitBtn.on("click", handleFormSubmit);
-  $exampleList.on("click", ".delete", handleDeleteBtnClick);
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+
+  
+  $("#finalTrans").click(function() {
+
+    console.log("clicked!");
+    buyCoins();
+
+
+
+  });
+
 });
