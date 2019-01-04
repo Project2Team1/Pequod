fe931570855b01764f4c694e86d3cf2a15da36d9
NiRuaG <gaurin1107@gmail.com>
2019-01-03 19:38:07 -0600

    axios module with test endpoint

--- private/cmcAPI.js
+++ private/cmcAPI.js
@@ -3,10 +3,30 @@
 const axios = require("axios");
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
+//TODO: custom header, timeout
+
 axios.get(url)
-  .then(function (response) {
-    console.log(response);
+
+  .then(function ({data, status, statusText, headers, config}={}) {
+    console.log(data);
   })
-  .catch(function (error) {
-    console.log(error);
+
+  .catch(function ({response, request, message, config}={}) {
+    console.log("CMC GET error");
+    //% https://www.npmjs.com/package/axios#handling-errors
+    if (response) {
+      // The request was made and the server responded with a status code that falls out of the range of 2xx
+      console.log("data:\n\t", response.data);
+      console.log("status:\n\t", response.status);
+      console.log("headers:\n\t", response.headers);
+    } else if (request) {
+      // The request was made but no response was received
+      // `request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log("\nrequest:\n\t", request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("\nError message:\n\t", message);
+    }
+    console.log("\nconfig:\n\t", config);
   });
