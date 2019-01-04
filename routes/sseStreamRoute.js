200b395626db32c945acb03cc1679b370bc7b286
NiRuaG <gaurin1107@gmail.com>
2019-01-03 22:10:45 -0600

    init wip

---
+++ routes/sseStreamRoute.js
@@ -0,0 +1,24 @@
+//% http://bayn.es/real-time-web-applications-with-server-sent-events-pt-1/
+//% https://stackoverflow.com/questions/34657222/how-to-use-server-sent-events-in-express-js
+
+const EventEmitter = require('events');
+const express = require('express');
+const app = express();
+
+const Stream = new EventEmitter(); // my event emitter instance
+
+app.get('/stream', function (request, response) {
+  response.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
+  Stream.on("push", function (event, data) {
+    response.write("event: " + String(event) + "\n" + "data: " + JSON.stringify(data) + "\n\n");
+  });
+});
+
+setInterval(function () {
+  Stream.emit("push", "test", { msg: "admit one" });
+}, 10000);
