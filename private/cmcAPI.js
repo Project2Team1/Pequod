6f6f7eb5b39d40f66b0570ede5d6ca91be7f3ac8
NiRuaG <gaurin1107@gmail.com>
2019-01-03 21:12:18 -0600

    setup axios for coinmarketcap (CMC) api calls (test /info endpoint)

--- private/cmcAPI.js
+++ private/cmcAPI.js
@@ -1,32 +1,47 @@
-// https://www.npmjs.com/package/axios
+require('dotenv').config();
 
-const axios = require("axios");
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+//% https://coinmarketcap.com/api/documentation/v1
+const cmcAPI = require('axios').create({
+  baseURL: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency',
+  headers: { 'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY },
+  timeout: 1000 * 10
+});
 
-//TODO: custom header, timeout
+// https://jsonplaceholder.typicode.com/posts/
+// require('axios')
+//   .get('https://jsonplaceholder.typicode.com/posts/1',
+//     })
 
-axios.get(url)
+
+cmcAPI.get('/info?',
+  {
+    params: {
+      symbol: ['BTC','ETH','XRP','XLM','LTC'].toString()
+    }
+  })
 
   .then(function ({data, status, statusText, headers, config}={}) {
     console.log(data);
+    console.log(status, statusText, headers, config);
   })
 
   .catch(function ({response, request, message, config}={}) {
-    console.log("CMC GET error");
     //% https://www.npmjs.com/package/axios#handling-errors
+    console.log("CMC GET error");
     if (response) {
       // The request was made and the server responded with a status code that falls out of the range of 2xx
-      console.log("data:\n\t", response.data);
-      console.log("status:\n\t", response.status);
+      console.log("data:\n\t"   , response.data   );
+      console.log("status:\n\t" , response.status );
       console.log("headers:\n\t", response.headers);
     } else if (request) {
       // The request was made but no response was received
-      // `request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
+      // `request` is an instance of http.ClientRequest in node.js
       console.log("\nrequest:\n\t", request);
     } else {
-      // Something happened in setting up the request that triggered an Error
+      // Something happened in setting up the request that triggered an error
       console.log("\nError message:\n\t", message);
     }
     console.log("\nconfig:\n\t", config);
-  });
+  })
+
+; // cmcAPI.get('/info?',
