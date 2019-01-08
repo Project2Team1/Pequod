a097992a95d76512bd3dc2f9b13334b9eb9d1db1
Andrew Gaurin <gaurin1107@gmail.com>
2019-01-08 15:22:21 -0600

    remove test route & cleanup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,37 +8,14 @@ router.get("/", async function(req, res) {
 
   let coins = 
     db.CryptoCoin
-      .findAll({})
-      .then(coinResults => coinResults.map(coin => { 
-        coin.value = 123;
-        return coin; }));
+      .findAll({});
 
  /*  let markets =
     db.CryptoMarket
       .findAll({}); */
 
   res.render("index", {
-    msg     : "Welcome!",
     coins   : await coins,
-    // examples: await examples,
-    // markets : await markets
-  });
-
-});
-
-router.get("/test", async function(req, res) {
-  
-  let coins = 
-    db.CryptoCoin
-      .findAll({})
-      .then(coinResults => coinResults.map(coin => { 
-        coin.value = 123;
-        return coin; }));
-
-  res.render("sseTest", {
-    msg     : "Welcome!",
-    coins   : await coins,
-    // examples: await examples,
     // markets : await markets
   });
 
@@ -50,14 +27,3 @@ router.get("*", function(req, res) {
 });
 
 module.exports = router;
-
- /*  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example
-      .findOne({ where: { id: req.params.id } })
-      .then(function(dbExample) {
-        res.render("example", {
-          example: dbExample
-        });
-      });
-  }); */
