569927cdd0e80241d2715a32daf5f8a0d3480a41
Andrew Gaurin <gaurin1107@gmail.com>
2019-01-09 14:39:02 -0600

    remove console log

--- public/js/index.js
+++ public/js/index.js
@@ -124,7 +124,6 @@ $(document).ready(function () {
       };
     });
 
-  // console.log(coins);
   // #endregion Variable Setup
 
 
@@ -177,7 +176,6 @@ $(document).ready(function () {
       coin.bank .valueEl.textContent = rnd;
     });
 
-    console.log(coins);
   }
 
   function buyCoins() {
@@ -196,7 +194,7 @@ $(document).ready(function () {
 
     if (transactionTotal > availableCash) {
       console.log("Purchase failed! Not enough cash.", transactionTotal, availableCash);
-      return alert("You can't spend money you don't have! Try again.");
+      return alert("You can't spend money you don't have! No Cheating. Try again.");
     }
 
     Object.values(coins).forEach(coin => {
